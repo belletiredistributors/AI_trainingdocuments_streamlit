270fc92c4d31319e21efc6f1a1dbc1b73a95bbb5
Sebastian Siolkowski <Sebastian.siolkowski@causewaybti.com>
2023-07-25 11:35:41 +0200

    speech to text v1

--- pdf_viewer_app.py
+++ pdf_viewer_app.py
@@ -4,6 +4,7 @@ import io
 import os
 from PIL import Image
 from utils.ChatGPT_pages_search import ChatGPT_pages_search
+from utils.speech_recognision import convert_speech_to_text
 
 pdf_file = os.path.join(os.getcwd(), 'static', 'pdfs', 'example.pdf')
 logo_file = os.path.join(os.getcwd(), 'static', 'img', 'BTlogo.jpg')
@@ -31,21 +32,37 @@ def main():
         # Load the PDF using PyMuPDF (fitz)
         pdf_document = fitz.open(stream=uploaded_file.read(), filetype="pdf")
 
-        # Show an input field for the user to enter their query
-        query = st.sidebar.text_input("Enter your query here:", "Please ask your question", key="input_query")
-        query = query + " on what page it was found?"
+        # Show a radio button for user to choose input type
+        input_type = st.sidebar.radio("Choose input type:", ("Text Input", "Speech Input"))
+
+        query = ""  # Initialize the query variable with an empty string
+
+        if input_type == "Text Input":
+            # Show a regular text input field
+            query = st.sidebar.text_input("Enter your query here:", "", key="input_query")
+
+        elif input_type == "Speech Input":
+            # Show the "Convert Speech to Text" button
+            if st.sidebar.button("Convert Speech to Text"):
+                text_from_speech = convert_speech_to_text()
+                query = text_from_speech if text_from_speech else ""
+
+        if query:
+            query = query + " on what page it was found?"
+            st.sidebar.write(f"Your query is: {query.replace(' on what page it was found?', '')}")
 
         # Create a submit button
         if st.sidebar.button("Submit"):
             search_engine = ChatGPT_pages_search(pdf_file)
+            print(f'Query before sending is {query}.')
             # Get information about the selected page using the custom function
             answer, page_numbers = query_ChatGPT(search_engine, query)
 
             # Show the GPT answer in the sidebar
             st.sidebar.markdown("## GPT Answer")
             st.sidebar.text_area("Answer:", answer, height=500, max_chars=5000)
-            
-            if page_numbers!= 'Page cannot be specified.':
+
+            if page_numbers != 'Page cannot be specified.':
                 for page_number in page_numbers:
                     st.sidebar.write(f"Page: {page_number}")
 
@@ -55,5 +72,6 @@ def main():
                     st.image(image_bytes, use_column_width=True, caption=f"Page {page_number}")
             else:
                 st.write(page_numbers)
+
 if __name__ == "__main__":
     main()
---
+++ utils/speech_recognision.py
@@ -0,0 +1,20 @@
+import speech_recognition as sr
+
+# Create a recognizer object
+r = sr.Recognizer()
+
+# Function to perform voice-to-text conversion
+def convert_speech_to_text():
+    # Use the default microphone as the audio source
+    with sr.Microphone() as source:
+        print("Speak something...")
+        audio = r.listen(source)
+    try:
+        # Use Google Speech Recognition to convert speech to text
+        text = r.recognize_google(audio)
+        print("You said:", text)
+        return text
+    except sr.UnknownValueError:
+        print("Sorry, I could not understand audio.")
+    except sr.RequestError as e:
+        print("Error occurred; {0}".format(e))
